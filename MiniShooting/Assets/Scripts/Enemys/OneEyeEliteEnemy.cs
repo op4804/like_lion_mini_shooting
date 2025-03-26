84ba4c10c9555dd4c3ceee198b51a7a901f28120
op4804 <op4804@gmail.com>
2025-03-26 17:59:12 +0900

    add eliteenemy pattern

--- MiniShooting/Assets/Scripts/Enemys/OneEyeEliteEnemy.cs
+++ MiniShooting/Assets/Scripts/Enemys/OneEyeEliteEnemy.cs
@@ -3,32 +3,47 @@ using UnityEngine;
 
 public class OneEyeEliteEnemy : EliteEnemy
 {
-    protected override void OnEnable()
+    protected override void OnEnable() // 초기화 및 시작
     {
-        currentEnemyHP = 5;
-        StartCoroutine(MoveFoward()); // 앞으로 가기
+        currentEnemyHP = 500;
+        StartCoroutine(MoveFoward());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    IEnumerator MoveFoward()
+    IEnumerator MoveFoward() // 앞으로 가기
     {
-        for(int i = 0; i < 100; i++)
+        for (int i = 0; i < 100; i++)
         {
             yield return new WaitForFixedUpdate();
             transform.Translate(Vector2.left * Time.deltaTime * 2f);
         }
 
-        StartCoroutine(SpecialAttack());
+        StartCoroutine(SpecialAttack()); // 특수 공격 시작
     }
 
     IEnumerator SpecialAttack()
     {
-        yield return new WaitForSeconds(5f);
-        // Instantiate();
+        while (true)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject go = Instantiate(ResourceManager.Instance.oneEyeEliteEnemyBullet, transform.position, Quaternion.identity);
+
+                float angle = 360 / 10 * i;
+
+                go.GetComponent<oneEyeEliteEnemyBullet>().Setdir(new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0));
+                go.GetComponent<oneEyeEliteEnemyBullet>().SetRot(angle);
+            }
+
+
+            yield return new WaitForSeconds(5f);
+
+
+        }
     }
 }
