3c6967cb5a61cd57e08da5840e3ccb8c60a6251d
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-03-28 13:31:19 +0900

    boss

--- MiniShooting/Assets/Scripts/BossScripts/BossPatternManager.cs
+++ MiniShooting/Assets/Scripts/BossScripts/BossPatternManager.cs
@@ -41,46 +41,99 @@ public class BossPatternManager : MonoBehaviour
 
     IEnumerator PatternProgress()
     {
-        StartCoroutine(patterns[0].PatternProgress());
-        yield return new WaitForSeconds(2f);
-        StartCoroutine(patterns[0].StopPattern());
-        yield return new WaitUntil(() => patterns[0].isPatternEnd);
+        //yield return StartCoroutine(Test(5f));
+        //패턴 테스트
 
-        StartCoroutine(patterns[1].PatternProgress());
-        yield return new WaitForSeconds(2f);
-        StartCoroutine(patterns[1].StopPattern());
-        yield return new WaitUntil(() => patterns[1].isPatternEnd);
+        yield return StartCoroutine(MaveAndShoot(5f));
+        yield return StartCoroutine(Sroad(5f));
+        yield return StartCoroutine(Rampage(5f));
+        yield return StartCoroutine(DashSlash(3f));
+        yield return StartCoroutine(SpinSword(5f));
+        yield return StartCoroutine(Lazer(5f));
+        yield return StartCoroutine(BlinkMove(5f));
+        yield return StartCoroutine(HealSword());
 
-        StartCoroutine(patterns[2].PatternProgress());
-        yield return new WaitForSeconds(2f);
-        StartCoroutine(patterns[2].StopPattern());
-        yield return new WaitUntil(() => patterns[2].isPatternEnd);
-
-        StartCoroutine(patterns[3].PatternProgress());
-        yield return new WaitForSeconds(2f);
-        StartCoroutine(patterns[3].StopPattern());
-        yield return new WaitUntil(() => patterns[3].isPatternEnd);
-
-        StartCoroutine(patterns[4].PatternProgress());
-        yield return new WaitForSeconds(2f);
-        StartCoroutine(patterns[4].StopPattern());
-        yield return new WaitUntil(() => patterns[4].isPatternEnd);
 
         StartCoroutine(patterns[5].PatternProgress());
-        yield return new WaitForSeconds(2f);
+        //패턴넣어주기
         StartCoroutine(patterns[5].StopPattern());
         yield return new WaitUntil(() => patterns[5].isPatternEnd);
 
         StartCoroutine(patterns[6].PatternProgress());
-        yield return new WaitForSeconds(2f);
+        //패턴넣어주기
         StartCoroutine(patterns[6].StopPattern());
-        yield return new WaitUntil(() => patterns[6].isPatternEnd);
-
-        StartCoroutine(patterns[7].PatternProgress());
-        yield return new WaitUntil(() => patterns[7].isPatternEnd);
+        yield return new WaitUntil(() => patterns[5].isPatternEnd);
     }
 
+    IEnumerator Test(float seconds)
+    {
+        StartCoroutine(patterns[0].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[0].StopPattern());
+        yield return new WaitUntil(() => patterns[0].isPatternEnd);
+    }
+    IEnumerator MaveAndShoot(float seconds)
+    {
+        StartCoroutine(patterns[1].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[1].StopPattern());
+        yield return new WaitUntil(() => patterns[1].isPatternEnd);
+        //무브엔슛
+    }
+    IEnumerator DashSlash(float seconds)
+    {
+        StartCoroutine(patterns[2].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[2].StopPattern());
+        yield return new WaitUntil(() => patterns[2].isPatternEnd);
+        //대쉬 슬래시
+    }
+    IEnumerator Sroad(float seconds)
+    {
+        StartCoroutine(patterns[3].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[3].StopPattern());
+        yield return new WaitUntil(() => patterns[3].isPatternEnd);
+        //s로드
+    }
+    IEnumerator Rampage(float seconds)
+    {
+        StartCoroutine(patterns[4].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[4].StopPattern());
+        yield return new WaitUntil(() => patterns[4].isPatternEnd);
+        //검기랜덤난사
+    }
 
-
-
+    IEnumerator SpinSword(float seconds)
+    {
+        StartCoroutine(patterns[5].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[5].StopPattern());
+        yield return new WaitUntil(() => patterns[5].isPatternEnd);
+        //회전검랜덤소환
+    }
+    IEnumerator Lazer(float seconds)
+    {
+        StartCoroutine(patterns[6].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[6].StopPattern());
+        yield return new WaitUntil(() => patterns[6].isPatternEnd);
+        //레이저랜덤소환
+    }
+    IEnumerator HealSword()
+    {
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(patterns[7].PatternProgress());
+        yield return new WaitUntil(() => patterns[7].isPatternEnd);
+        //봉인검소환,부실때까지회복
+    }
+    IEnumerator BlinkMove(float seconds)
+    {
+        StartCoroutine(patterns[8].PatternProgress());
+        yield return new WaitForSeconds(seconds);
+        StartCoroutine(patterns[8].StopPattern());
+        yield return new WaitUntil(() => patterns[6].isPatternEnd);
+        //사라졋다가 나타나서 쏘기반복
+    }
 }
